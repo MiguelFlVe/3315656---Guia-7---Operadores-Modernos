9773439b5ed75e906a9d9a9fcad56425f9ec1e97
Oscar <mao090997@gmail.com>
2026-04-16 12:35:28 -0500

    fix: se resuelven unas irregularidades cometidas por el desarrolador oscar solano

--- app.js
+++ app.js
@@ -12,9 +12,6 @@ import evaluar from './Apropiación/Ej7.js';
 import fusionarColecciones from './Apropiación/Ej8.js';
 
 // Ejercicios de Transferencia
-
-//Ejercicio 1: Sistema de registro académico
-import { crearEstudiante } from "./Transferencia/Ej1.js";
 // Ejercicio 5: Motor de configuración avanzada
 import configFinal from './Transferencia/Ej5.js';
 
@@ -32,7 +29,6 @@ const Aprop = {
 };
 
 const Transf = {
-    Ej1: "Sistema de registro académico",
     Ej5: "Motor de configuración avanzada"
 };
 
@@ -211,24 +207,13 @@ while (condition) {
 
             console.log("Has seleccionado Transferencia");
             
-            console.log(`Seleccione el ejercicio a revisar: \n 1. ${Ej1} \n 5. ${Ej5} \n 6. Salir`);
+            console.log(`Seleccione el ejercicio a revisar: \n 5. ${Ej5} \n 6. Salir`);
 
             let ejTransf = prompt();
 
             switch (ejTransf) {
-                // Ejercicio 1: Sistema de registro académico
-                case "1":
-                    console.log("----------------------------------------");
-
-                    console.log(`Has seleccionado el ejercicio ${Ej1}`);
-
-                    const estudiante = crearEstudiante("Oscar", 4, 3, 5);
-
-                    console.log(estudiante);
-
-                    break
-                    
-                // Ejercicio 5: Motor de configuración avanzada
+                
+               // Ejercicio 5: Motor de configuración avanzada
                 case "5":
                     console.log("----------------------------------------");
 
